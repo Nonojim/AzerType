cdb0a2883a1e1100cf6e5d7b190b0331aa70b8a2
Nonojim <nolan.pederencino@gmail.com>
2023-08-08 10:23:05 +0200

    importation dossier

--- scripts/config.js
+++ scripts/config.js
@@ -1,2 +1,10 @@
-const Listmot = ["Cachalot", "Requin", "Dauphin"]
-const Listphrases = ["Pas de panique !", "La vie, l'univers et le reste", "Merci pour le poisson"]
+/*********************************************************************************
+ * 
+ * Ce fichier contient toutes les constantes nécessaires au fonctionnement du jeu.
+ * En particulier les listes de mots et de phrases proposés à l'utilisateur
+ * 
+ *********************************************************************************/
+
+// Déclaration des tableaux contenant les listes des mots proposés à l'utilisateur
+const listeMots = ["Cachalot", "Pétunia", "Serviette"]
+const listePhrases = ["Pas de panique !", "La vie, l'univers et le reste", "Merci pour le poisson"]
